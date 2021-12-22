5434f9c778e14c6ca12103b8c712276de27524bd
rahulkudala <96419136+rahulkudala@users.noreply.github.com>
2021-12-22 09:51:08 +0530

    Add files via upload

---
+++ Student1/src/com/cjss/rahul/Main.java
@@ -0,0 +1,22 @@
+package com.cjss.rahul;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        Student s1 = new Student();
+      //  System.out.print("Enter No. of Students: ");
+
+
+        Object st = new Object();
+        int n = s.nextInt();
+        for (int i=1;i<=n;i++) {
+            st[i] = ;
+        }
+        s1.setName();
+
+	// write your code here
+    }
+}
